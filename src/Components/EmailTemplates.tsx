ce3c61adab2bb82bd958b116e53fea780fb04400
Bsc-40-20 <bsc-40-20@unima.ac.mw>
2023-06-10 23:14:40 +0200

    templates-init

--- src/Components/EmailTemplates.tsx
+++ src/Components/EmailTemplates.tsx
@@ -1,8 +1,13 @@
-
 const EmailTemplates = () => {
   return (
-    <div className="">EmailTemplates</div>
-  )
-}
+    <div className="py-6">
+      <h2 className="text-center bg-clip-text text-transparent bg-gradient-to-l from-green-800 to-green-400 text-[1.7rem] sm:text-6xl font-bold select-none">Browse templates</h2>
 
-export default EmailTemplates
+      <div>
+
+      </div>
+    </div>
+  );
+};
+
+export default EmailTemplates;
